5d0aa99de34f007f97673c64cbecf96b5de03eee
giannis <bkgiannis8@gmail.com>
2022-10-20 02:00:31 +0300

    Add operators to display and display function

--- calculator.js
+++ calculator.js
@@ -44,11 +44,42 @@ const clearButton = document.querySelector(".clear");
 const equalButton = document.querySelector(".equal");
 const calcDisplay = document.querySelector(".screen");
 calcDisplay.textContent=0
-let total=""
+let storedNumber = '';
+let clickedOperator = ''
+let firstNumber = '';
+let result = '';
 
 numberButton.forEach((number => {
     number.addEventListener("click", function() {
-        total += number.id;
-        calcDisplay.textContent = total
+        storedNumber += number.id;
+        calcDisplay.textContent = storedNumber
     })
 }))
+
+operatorButton.forEach((operator => {
+    operator.addEventListener("click", function() {
+        if (firstNumber && storedNumber) {
+            displayResult();
+          }
+          // save the first number
+          firstNumber = storedNumber;
+      
+          // get the operator that was clicked
+          clickedOperator = operator.textContent;
+          previousOperand.textContent = storedNumber + clickedOperator;
+          storedNumber = '';
+    })
+}));
+
+equalButton.addEventListener('click', function() {
+    displayResult();
+  });
+
+function displayResult() {
+result = operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator)
+// update content of current operation with result and previous operand with the calculation, make storedNumber = result
+currentOperand.textContent = result;
+previousOperand.textContent = firstNumber + ' ' + clickedOperator + ' ' + storedNumber;
+storedNumber = result;
+console.log('FirstNumber' + firstNumber + 'Stored' + storedNumber);
+}
