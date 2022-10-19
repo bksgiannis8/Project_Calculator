c0a6ac12eddc2460fdc88e764987d790b5997aae
giannis <bkgiannis8@gmail.com>
2022-10-19 17:46:24 +0300

    Create functions for operators

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function addNumbers(number1, number2) {
+    const addition = number1 + number2;
+    return addition;
+}
+
+function subtractNumbers(number1, number2) {
+    const subtraction = number1 - number2;
+    return subtraction;
+}
+
+function multiplyNumbers(number1, number2) {
+    const multiplication = number1 * number2;
+    return multiplication
+}
+
+function divideNumbers(number1, number2) {
+    const division = number1 / number2;
+    return division
+}
